23bd61f8c94fc35edaa52940819c8ccee4b8fd0b
github username <github email address>
2020-12-01 16:29:46 -0600

    Navbar links and headeer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,38 @@
 import React from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      I'm the Navbar
-    </div>
+    <Router>
+          <div className="navbar-row">
+          <h1 className="bio-text">Smooth Waggie LTD</h1>
+          <Link to="/">Home</Link>&nbsp;&nbsp;&nbsp;
+          <Link to="/About">About</Link>&nbsp;&nbsp;&nbsp;
+          <Link to="/Products">Products</Link>&nbsp;&nbsp;&nbsp;
+          <Link to="/Reviews">Reviews</Link>&nbsp;&nbsp;&nbsp;
+          <Link to="/Logout">Logout</Link>
+          </div>
+          {/* <Switch>
+            <Route exact path="/">
+               <Bio/>
+            </Route>
+            <Route exact path="/GuitarArchives">
+             <GuitarContainer/>
+            </Route>
+            <Route exact path="/Demos">
+             <Demos/>
+            </Route>
+            <Route exact path="/Workshop">
+             <Workshop/>
+            </Route>
+          </Switch> */}
+          
+        </Router> 
   )
 }
 
