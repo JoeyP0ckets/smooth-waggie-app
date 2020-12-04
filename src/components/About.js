ea53de836b664de74e6933197baeb1b773262ab2
github username <github email address>
2020-12-04 08:56:23 -0600

    about statement

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react"
 const About = () => {
   return(
     <div>
-      I'm the about page
+      Smooth Waggie products are made from locally sourced, natural materials.  All cloth is made from hemp, a plant known for producing resilient, comfortable fabric.  Hemp was grown as a cash crop in the early United States
     </div>
   )
 }
