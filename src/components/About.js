da1d91416f15d4fbb57bc0888ff71a73f3d130a2
github username <github email address>
2020-12-04 11:07:32 -0600

    changed some styling for about

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,10 @@
 import React from "react"
+import AboutContainer from "../containers/AboutContainer"
 
 const About = () => {
   return(
-    <div>
-      Smooth Waggie products are made from locally sourced, natural materials.  All cloth is made from hemp, a plant known for producing resilient, comfortable fabric.  Hemp was grown as a cash crop in the early United States
+    <div className="about-container">
+      <AboutContainer/>
     </div>
   )
 }
---
+++ src/containers/AboutContainer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AboutContainer = () => {
+  return(
+  <div className="about-text">
+    Smooth Waggie products are made from locally sourced, natural materials.  All cloth is made from hemp, a plant known for producing resilient, comfortable fabric.  Hemp was grown as a cash crop in the early United States
+  </div>
+  )
+}
+
+export default AboutContainer
